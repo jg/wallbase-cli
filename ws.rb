cbdca15275d0f8a046b01bc5944e31f80463e7dd
Juliusz Gonera <juliusz.gonera@gmail.com>
2011-07-15 05:46:25 +0200

    Working version

--- ws.rb
+++ ws.rb
@@ -18,11 +18,9 @@ class WallbaseScraper
     end
   end
 
-  def initialize
-    @agent ||= Mechanize.new
-
+  def login
     url = 'http://wallbase.cc/user/login_form'
-    page = @agent.get(url)
+    page = agent.get(url)
 
     form = page.form_with(:action => 'http://wallbase.cc/user/login')
 
@@ -30,6 +28,10 @@ class WallbaseScraper
     form.pass = 'eephaix8'
 
     form.submit
+  end
+
+  def logged_in?
+    agent.cookies.to_s =~ /wallbase_session/
   end
 
   def page_url(id)
@@ -41,11 +43,13 @@ class WallbaseScraper
 
   # Returns array of Album objects 
   def albums
+    login if not logged_in?
+
     albums = []
     albums << Album.new('Home', 'http://wallbase.cc/user/favorites/-1')
 
     url = 'http://wallbase.cc/user/favorites/0'
-    page = @agent.get(url)
+    page = agent.get(url)
 
     regexp = /<li class=\"sortable\".+?<a href=\"(.+?)\".+?<\/span>(.+?)<\/a>/
     page.body.scan(regexp).each do |result|
@@ -57,7 +61,9 @@ class WallbaseScraper
 
   # Returns list 
   def image_urls(url)
-    page = @agent.get(url)
+    login if not logged_in?
+
+    page = agent.get(url)
 
     urls = []
     links = page.links.select {|l| l.href =~ /wallbase.cc\/wallpaper/ } 
@@ -70,20 +76,45 @@ class WallbaseScraper
     return urls
   end
 
-  def agent
-    @agent
+  def method_name
   end
-# images = page.images.select {|img| img.src =~ /wallbase1\.org/ }
-# image_ids = images.map {|img| img.src.match(/thumb-(\d+)\.jpg/)[1]}
-# puts image_ids
+
+  def log(txt)
+    puts "#{Time.now} #{txt}" if $DEBUG
+  end
+
+  # Writes found image urls to stdout, should be quicker in conjunction
+  # with eg wget
+  def incremental_image_urls(url)
+    log "method begin" if $DEBUG
+    login if not logged_in?
+    log "logged in" if $DEBUG
+
+    page = agent.get(url)
+    log "got page" if $DEBUG
+
+    links = page.links.select {|l| l.href =~ /wallbase.cc\/wallpaper/ } 
+    log "selected links" if $DEBUG
+    links.each do |l|
+      page = l.click
+      log "clicked link" if $DEBUG
+      url = page.image_urls().select {|e| e.match /wallbase2/ }.first
+      puts url
+    end
+
+  end
+
+  def agent
+    @agent ||= Mechanize.new
+  end
 
 end
-
 
 opts = Trollop::options do
   banner "Wallbase.cc CLI utility"
   opt :list_albums, "List albums", :short => 'l', :default => false
-  opt :get_image_urls, "Get list of image urls for a given album", :short => 'g', :type => String
+  opt :get_image_urls, "Get list of image urls for a given album url", :short => 'g', :type => String
+  opt :get_incremental_image_urls, "Get incremental list of image urls for a given album url", :short => 'i', :type => String
 end
 
 s = WallbaseScraper.new
@@ -96,3 +127,6 @@ if opts[:get_image_urls]
   puts s.image_urls(opts[:get_image_urls])
 end
 
+if opts[:get_incremental_image_urls]
+  puts s.incremental_image_urls(opts[:get_incremental_image_urls])
+end
